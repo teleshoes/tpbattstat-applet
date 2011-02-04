367f574b136a76d3367f571f22baa39c49c442c2
Elliot Wolk <elliot.wolk@gmail.com>
2011-02-03 19:23:59 -0500

    separated HUD into a separate file

---
+++ src/tpbattstat-display.h
@@ -0,0 +1,28 @@
+/**************************************************************************
+ *  TPBattStatApplet v0.1
+ *  Copyright 2011 Elliot Wolk
+ **************************************************************************
+ *  This file is part of TPBattStatApplet.
+ *
+ *  TPBattStatApplet is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  TPBattStatApplet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with TPBattStatApplet.  If not, see <http://www.gnu.org/licenses/>.
+ *************************************************************************/
+
+#ifndef TPBATTSTAT_DISPLAY_H
+#define TPBATTSTAT_DISPLAY_H
+
+#include <panel-applet.h>
+
+void update_display (TPBattStat *tpbattstat);
+
+#endif /* TPBATTSTAT_DISPLAY_H */
