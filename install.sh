bac652cc11f348d463bf4b043bb1d4bafe7ab95c
Elliot Wolk <elliot.wolk@gmail.com>
2011-05-05 17:41:17 -0400

    started conversion to python

--- install.sh
+++ install.sh
@@ -5,28 +5,9 @@ BIN_DIR=/usr/bin
 ICON_DIR=/usr/share/pixmaps
 SERVER_DIR=/usr/lib/bonobo/servers
 
-DEPS=libpanelapplet-2.0
-CFLAGS=`pkg-config --cflags $DEPS`
-LIBS=`pkg-config --libs $DEPS`
-
-cd src
-for srcfile in `ls *.c`; do
-  echo compiling $srcfile to object
-  echo
-  gcc $CFLAGS $LIBS -g -O2 -c $srcfile
-done
-
-echo compiling $NAME from objects
+echo copying $NAME.py to $BIN_DIR
 echo
-gcc -g -O2 -o $NAME *.o $LIBS
-
-echo installing $NAME to $BIN_DIR
-echo
-sudo cp $NAME $BIN_DIR
-
-echo cleanup binary and objects
-echo
-rm $NAME *.o
+sudo cp $NAME.py $BIN_DIR
 
 cd ../
 
