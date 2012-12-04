1e19ffc0045ba5c6361779eb388011be337ce396
Elliot Wolk <elliot.wolk@gmail.com>
2012-12-04 01:24:56 -0500

    removed gnome-applet installation and config

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 NAME=tpbattstat-applet
 LIB_INSTALL_DIR=/usr/lib/$NAME
 ICON_DIR=/usr/share/pixmaps
-SERVER_DIR=/usr/lib/bonobo/servers
 
 sudo cp tpacpi-bat $LIB_INSTALL_DIR
 cd src
@@ -26,20 +25,3 @@ sudo rm -rf $ICON_DIR/$NAME/
 sudo mkdir $ICON_DIR/$NAME/
 sudo cp -ar * $ICON_DIR/$NAME/
 cd ../
-
-echo installing gconf schemas
-echo
-gconftool-2 --install-schema-file tpbattstat-applet.schemas
-
-echo applying schemas to the default key dir {not-in-gnome-panel}
-PREFS_DIR=/apps/tpbattstat_applet/prefs
-SCHEMA_DIR=/schemas$PREFS_DIR
-SCHEMAS=`gconftool-2 -a $SCHEMA_DIR | \
-         sed 's/^ \([a-zA-Z0-9_]\+\) = .*$/\1/'`
-for s in $SCHEMAS; do
-  gconftool-2 --apply-schema $SCHEMA_DIR/$s $PREFS_DIR/$s
-done
-
-echo installing bonobo server
-echo
-sudo cp TPBattStatApplet_Factory.server $SERVER_DIR
--- uninstall.sh
+++ uninstall.sh
@@ -3,14 +3,8 @@
 NAME=tpbattstat-applet
 LIB_INSTALL_DIR=/usr/lib/$NAME
 ICON_DIR=/usr/share/pixmaps
-SERVER_DIR=/usr/lib/bonobo/servers
 
 sudo rm -rf $LIB_INSTALL_DIR
 sudo rm -rf $ICON_DIR/$NAME
 
 sudo ./smapi-battaccess/uninstall-smapi-battaccess.sh
-
-gconftool-2 --recursive-unset /apps/tpbattstat_applet
-gconftool-2 --recursive-unset /schemas/apps/tpbattstat_applet
-
-sudo rm $SERVER_DIR/TPBattStatApplet_Factory.server
